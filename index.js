61d61d28c6ab4abbdae27cb60d7ed7d14befe77a
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-11-18 18:02:45 +0500

    Implement timeouts

--- index.js
+++ index.js
@@ -9,16 +9,45 @@ function Reque() {
 
 	PassThrough.call(this);
 
-	this.defaultTimeout = 60 * 1000;
+	this._timeout = 2 * 60 * 1000;
+
+	var self = this;
+	this._timeoutCb = function (req) {
+		req.abort();
+		self.emit('error', new Error('Request timeout. Aborting.'), req);
+	};
+
 }
 
 Reque.prototype = Object.create(PassThrough.prototype);
 
-Reque.prototype.request = function request () {
+Reque.prototype.request = function request (req) {
+
+	this.emit(this._req ? 'redirect' : 'request', req);
+
+	this._req = req;
+
+	var self = this;
+
+	process.nextTick(function () {
+		var timer = setTimeout(self._timeoutCb, self._timeout, self._req);
+
+		req.on('response', function () {
+			clearTimeout(timer);
+		})
+	});
+
 	return this;
 };
 
-Reque.prototype.timeout = function timeout () {
+Reque.prototype.timeout = function timeout (ms, cb) {
+
+	this._timeout = ms;
+
+	if (cb) {
+		this._timeoutCb = cb;
+	}
+
 	return this;
 };
 
--- test.js
+++ test.js
@@ -1,10 +1,16 @@
-/* global it */
+/* global describe, it, before, after */
 
 'use strict';
 
 var Reque = require('./index.js');
 var PassThrough = require('stream').PassThrough;
 var assert = require('assert');
+
+var http = require('http');
+
+function req(url) {
+	return http.get(url || 'http://google.com');
+}
 
 it('should create Reque without new', function () {
 	assert.ok(Reque());
@@ -13,4 +19,69 @@ it('should create Reque without new', function () {
 it('should create instance of PassThrough stream', function () {
 	var reque = new Reque();
 	assert.ok(reque instanceof PassThrough);
+});
+
+it('should emit request on first call', function (done) {
+	var reque = new Reque();
+
+	var first = req();
+
+	reque.on('request', function (req) {
+		assert.equal(req, first);
+		done();
+	});
+
+	reque.on('redirect', done);
+
+	reque.request(first);
+});
+
+it('should emit redirect on second call', function (done) {
+	var reque = new Reque();
+
+	var first = req();
+	var second = req();
+
+	reque.on('redirect', function (req) {
+		assert.equal(req, second);
+		done();
+	});
+
+	reque.request(first);
+	reque.request(second);
+});
+
+describe('timeout', function () {
+	var server;
+
+	before(function (done) {
+		server = http.createServer();
+		server.listen(1234, done);
+	});
+
+	after(function (done) {
+		server.close();
+		done();
+	});
+
+	it('should emit error', function (done) {
+		var reque = new Reque();
+
+		var first = req('http://0.0.0.0:1234');
+
+		reque.on('error', function (err, req) {
+			try {
+				assert.ok(err);
+				assert.equal(err.message, 'Request timeout. Aborting.');
+				assert.equal(req, first);
+				done();
+			} catch (err) {
+				done(err);
+			}
+		});
+
+		reque
+			.request(first)
+			.timeout(1);
+	});
 });
